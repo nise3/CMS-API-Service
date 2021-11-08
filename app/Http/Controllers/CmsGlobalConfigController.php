a56176dcd7a99730ad4f3c468a63bd6e66f769ea
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2021-11-08 14:41:08 +0600

    Added cms global config response

--- app/Http/Controllers/CmsGlobalConfigController.php
+++ app/Http/Controllers/CmsGlobalConfigController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\Common\CmsGlobalConfigService;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Response;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
@@ -11,12 +12,16 @@ class CmsGlobalConfigController extends Controller
 {
     public CmsGlobalConfigService $cmsGlobalConfigService;
 
+    private Carbon $startTime;
+
     /**
      * @param CmsGlobalConfigService $cmsGlobalConfigService
+     * @param Carbon $startTime
      */
-    public function __construct(CmsGlobalConfigService $cmsGlobalConfigService)
+    public function __construct(CmsGlobalConfigService $cmsGlobalConfigService,Carbon $startTime)
     {
         $this->cmsGlobalConfigService = $cmsGlobalConfigService;
+        $this->startTime = $startTime;
     }
 
     /**
@@ -24,7 +29,15 @@ class CmsGlobalConfigController extends Controller
      */
     public function getGlobalConfig(): JsonResponse
     {
-        $response = $this->cmsGlobalConfigService->getGlobalConfigList();
-        return Response::json($response, ResponseAlias::HTTP_OK);
+        $response = [
+            '_response_status' => [
+                'data' => $this->cmsGlobalConfigService->getGlobalConfigList(),
+                "success" => false,
+                "code" => ResponseAlias::HTTP_UNPROCESSABLE_ENTITY,
+                "message" => "User is not created",
+                "query_time" => $this->startTime->diffInSeconds(Carbon::now()),
+            ]
+        ];
+        return Response::json($response, $response['_response_status']['code']);
     }
 }
