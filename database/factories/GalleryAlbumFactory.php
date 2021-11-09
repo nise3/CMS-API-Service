228dc98b2e164a734fdf3a5b095040ec8953dd95
Md Tasmidur Rahman <tasmidurrahman@gmail.com>
2021-11-09 18:33:52 +0600

    Cross api calling for get title

--- database/factories/GalleryAlbumFactory.php
+++ database/factories/GalleryAlbumFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\BaseModel;
 use App\Models\GalleryAlbum;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class GalleryAlbumFactory extends Factory
@@ -18,6 +19,7 @@ class GalleryAlbumFactory extends Factory
             'title' => ucfirst($title),
             'show_in' => $this->faker->randomElement(array_keys(BaseModel::SHOW_INS)),
             'album_type' => $this->faker->randomElement(GalleryAlbum::GALLERY_ALBUM_TYPES),
+            'published_at' => Carbon::now(),
             'organization_id' => $this->faker->numberBetween(1, 10),
             'industry_association_id' => $this->faker->numberBetween(1, 10),
             'batch_id' => $this->faker->numberBetween(1, 10),
