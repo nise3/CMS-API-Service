b5057c95519492185b5d1bff8a83671c907b32dc
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2021-11-11 11:18:38 +0600

    Added row_status in recent Activities

--- app/Http/Controllers/RecentActivityController.php
+++ app/Http/Controllers/RecentActivityController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 
-use App\Http\Resources\GalleryImageVideoResource;
 use App\Http\Resources\RecentActivityResource;
 use App\Models\BaseModel;
 use App\Models\RecentActivity;
