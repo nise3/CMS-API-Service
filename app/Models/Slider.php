d7ae3421617763f87b8d749f192d29f5f946d3a8
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2021-11-14 16:40:29 +0600

    Done added slider public list

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\Scopes\ScopeRowStatusTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
@@ -23,5 +24,11 @@ class Slider extends Model
 
     protected $guarded = BaseModel::COMMON_GUARDED_FIELDS_SOFT_DELETE;
 
-
+    /**
+     * @return HasMany
+     */
+    public function banners(): HasMany
+    {
+        return $this->hasMany(Banner::class, 'slider_id', 'id');
+    }
 }
