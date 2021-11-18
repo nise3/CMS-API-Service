df084d969e0d6627b40030f60d4af1a879e6901d
taskinur rahman <taskinur.rahman2014@gmail.com>
2021-11-18 11:44:14 +0600

    completed StaticPageTypeSeeder.php

--- database/seeders/StaticPageTypeSeeder.php
+++ database/seeders/StaticPageTypeSeeder.php
@@ -2,11 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\CmsLanguage;
-use App\Models\RecentActivity;
-use App\Models\StaticPageType;
+
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
@@ -30,11 +27,10 @@ class StaticPageTypeSeeder extends Seeder
             array('id' => '3', 'category' => 2, 'title_en' => 'How Nise3 Works', 'title' => 'নাইস-৩ কিভাবে কাজ করে', 'type' => 1, 'page_code' => 'how-nise3-works', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()),
             array('id' => '4', 'category' => 2, 'title_en' => 'Self Assessment', 'title' => 'নিজেকে যাচাই করুন', 'type' => 1, 'page_code' => 'self-assessment', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()),
             array('id' => '5', 'category' => 4, 'title_en' => 'Institute details', 'title' => 'প্রতিষ্ঠানের বিস্তারিত', 'type' => 1, 'page_code' => 'institute_details', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()),
-            array('id' => '6', 'category' => 1, 'title_en' => 'শর্তাবলী', 'title' => 'Terms and conditions', 'type' => 2, 'page_code' => ' terms-and-conditions', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()),
-            array('id' => '7', 'category' => 2, 'title_en' => 'How Nise3 Works', 'title' => 'নাইস-৩ কিভাবে কাজ করে', 'type' => 1, 'page_code' => 'how-nise3-works', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()),
-            array('id' => '8', 'category' => 2, 'title_en' => 'How Nise3 Works', 'title' => 'নাইস-৩ কিভাবে কাজ করে', 'type' => 1, 'page_code' => 'how-nise3-works', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()),
+            array('id' => '6', 'category' => 1, 'title_en' => 'Terms and conditions', 'title' => 'শর্তাবলী', 'type' => 2, 'page_code' => ' terms-and-conditions', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()),
+            array('id' => '7', 'category' => 1, 'title_en' => 'Career Advice ', 'title' => 'ক্যারিয়ার পরামর্শ', 'type' =>2, 'page_code' => 'career-advice', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()),
+            array('id' => '8', 'category' => 2, 'title_en' => 'Guidelines', 'title' => 'নির্দেশিকা', 'type' => 2, 'page_code' => 'guidelines', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()),
         ));
-        career-advice -> type(2)-> category-1
         Schema::enableForeignKeyConstraints();
     }
 }
