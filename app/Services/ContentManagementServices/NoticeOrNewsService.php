59eda68f481fb28866f540d28fe65bedbabf2fe4
rrahul <rahulbgc21@gmail.com>
2021-12-22 09:58:44 +0600

    auth service provider

--- app/Services/ContentManagementServices/NoticeOrNewsService.php
+++ app/Services/ContentManagementServices/NoticeOrNewsService.php
@@ -29,8 +29,6 @@ class NoticeOrNewsService
      */
     public function getNoticeOrNewsServiceList(array $request, $startTime = null): Collection|LengthAwarePaginator|array
     {
-        $authUser = Auth::user();
-        dd($authUser->isIndustryAssociationUser());
         $searchText = $request['search_text'] ?? "";
         $titleEn = $request['title_en'] ?? "";
         $titleBn = $request['title'] ?? "";
